366512b5fd8300b314120a1fcdb19e468bdc2972
Abdurashid <aj6243434@gmail.com>
2024-12-20 23:57:46 +0500

    tab finish

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 const tabBtns = document.querySelectorAll(".tab__nav-link");
+const tabInnerBtns = document.querySelectorAll(".tab__inner-link");
 
-tabBtns.forEach((item) => {
+function activeTab(btn) {
+  btn.forEach((item) => {
+    item.addEventListener("click", () => {
+      btn.forEach((item) => {
+        item.classList.remove("active");
+      });
+      item.classList.add("active");
+    });
+  });
+}
+activeTab(tabBtns);
+activeTab(tabInnerBtns);
+
+const tabContentItems = document.querySelectorAll(".tab__content-item");
+tabBtns.forEach((item, index) => {
   item.addEventListener("click", () => {
-    tabBtns.forEach((item) => {
+    tabContentItems.forEach((item) => {
       item.classList.remove("active");
     });
-    item.classList.add("active");
+    tabContentItems[index].classList.add("active");
   });
 });
