cb6ebee4e382417923e384e4aad9e3cde63bcc4e
Abdurashid <aj6243434@gmail.com>
2024-12-21 13:29:17 +0500

    finish

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
+//-------------VARIABLES-------------------
 const tabBtns = document.querySelectorAll(".tab__nav-link");
 const tabContentItems = document.querySelectorAll(".tab__content-item");
 const tabInnerBtns = document.querySelectorAll(".tab__inner-link");
 const tabInnerContentItems = document.querySelectorAll(
   ".tab__inner__content-item"
 );
-
+const tabMobileBtn = document.querySelector(".tab__btn-sm");
+const tabMobileContent = document.querySelector(".tab__nav");
+const tabCloseBtn = document.querySelector(".tab__btn-close");
+const body = document.querySelector("body");
+//--------------Main-Tab--------------------------------------------------
 tabBtns.forEach((item, index) => {
   item.addEventListener("click", () => {
+    tabMobileContent.classList.remove("active");
+    body.classList.remove("shadow");
+    tabMobileBtn.classList.remove("active");
     tabBtns.forEach((btn) => btn.classList.remove("active"));
     item.classList.add("active");
     tabContentItems.forEach((contentItem) =>
@@ -15,7 +23,7 @@ tabBtns.forEach((item, index) => {
     tabContentItems[index].classList.add("active");
   });
 });
-//----------------------------------------------------------------
+//--------------Inner-Tab--------------------------------------------------
 tabInnerBtns.forEach((item, index) => {
   item.addEventListener("click", () => {
     tabInnerBtns.forEach((btn) => btn.classList.remove("active"));
@@ -26,11 +34,15 @@ tabInnerBtns.forEach((item, index) => {
     tabInnerContentItems[index].classList.add("active");
   });
 });
-// tabInnerContentItems.forEach((item, index) => {
-//   item.addEventListener("click", () => {
-//     tabInnerContentItems.forEach((contentItem) =>
-//       contentItem.classList.remove("active")
-//     );
-//     item.classList.add("active");
-//   });
-// });
+//---------------Mobile-Tab-------------------------------------------------
+
+tabMobileBtn.addEventListener("click", () => {
+  tabMobileContent.classList.toggle("active");
+  body.classList.add("shadow");
+  tabMobileBtn.classList.toggle("active");
+});
+tabCloseBtn.addEventListener("click", () => {
+  tabMobileContent.classList.remove("active");
+  body.classList.remove("shadow");
+  tabMobileBtn.classList.remove("active");
+});
