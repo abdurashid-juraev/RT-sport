adc17e1cc5b1dfccdc85aaa852e9e7c8bcab1b5f
Abdurashid <aj6243434@gmail.com>
2024-12-21 09:54:31 +0500

    inner tab finish

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
 const tabBtns = document.querySelectorAll(".tab__nav-link");
-const tabInnerBtns = document.querySelectorAll(".tab__inner-link");
-
-function activeTab(btn) {
-  btn.forEach((item) => {
-    item.addEventListener("click", () => {
-      btn.forEach((item) => {
-        item.classList.remove("active");
-      });
-      item.classList.add("active");
-    });
-  });
-}
-activeTab(tabBtns);
-activeTab(tabInnerBtns);
-
 const tabContentItems = document.querySelectorAll(".tab__content-item");
+const tabInnerBtns = document.querySelectorAll(".tab__inner-link");
+const tabInnerContentItems = document.querySelectorAll(
+  ".tab__inner__content-item"
+);
+
 tabBtns.forEach((item, index) => {
   item.addEventListener("click", () => {
-    tabContentItems.forEach((item) => {
-      item.classList.remove("active");
-    });
+    tabBtns.forEach((btn) => btn.classList.remove("active"));
+    item.classList.add("active");
+    tabContentItems.forEach((contentItem) =>
+      contentItem.classList.remove("active")
+    );
     tabContentItems[index].classList.add("active");
   });
 });
+//----------------------------------------------------------------
+tabInnerBtns.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    tabInnerBtns.forEach((btn) => btn.classList.remove("active"));
+    item.classList.add("active");
+    tabInnerContentItems.forEach((contentItem) =>
+      contentItem.classList.remove("active")
+    );
+    tabInnerContentItems[index].classList.add("active");
+  });
+});
+// tabInnerContentItems.forEach((item, index) => {
+//   item.addEventListener("click", () => {
+//     tabInnerContentItems.forEach((contentItem) =>
+//       contentItem.classList.remove("active")
+//     );
+//     item.classList.add("active");
+//   });
+// });
